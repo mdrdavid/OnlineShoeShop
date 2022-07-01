b04a7d0ac893101677e6ccc6da3c6297278ccb09
david <davidmatovu88@gmail.com>
2022-07-01 22:45:58 +0300

    Not found page

---
+++ src/pages/notfound/NotFound.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../../components/profileComponents/mainheader/Header'
+import { Link } from 'react-router-dom'
+
+const  NotFound= ()=> {
+  return (
+      <>
+    <div className='container'>
+        <div className='image'>
+            <h4 className='text-center'>Page Not Found</h4>
+            <img
+            style ={{width: "100%", height: "300px", objective: "contain"}}
+            src=""
+            alt="Not-found"
+            />
+            <button className='btn-success'>
+                <Link to="/" className='text-white text-decoration- none'>
+                    Home page
+                </Link>
+            </button>
+        </div>
+    </div>
+    </>
+  )
+}
+export default NotFound
