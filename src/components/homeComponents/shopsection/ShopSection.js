a33c4e74f5c05aa5c03406f73df0223d74b387d0
david <davidmatovu88@gmail.com>
2022-07-03 09:55:48 +0300

    Edit shopSection

--- src/components/homeComponents/shopsection/ShopSection.js
+++ src/components/homeComponents/shopsection/ShopSection.js
@@ -2,38 +2,39 @@ import React from 'react'
 import Rating from '../Rating'
 import Pagination from '../Pagination'
 import { Link } from 'react-router-dom'
+import Products from '../../data/Products'
 
  const  ShopSection= ()=> {
-     const products =["sneakers", "jeans", "sandles"]
+    //  const products =["sneakers", "jeans", "sandles"]
   return (
     <div className ="container">
         <div className='section'>
             <div className='row'>
                 <div className='col-lh-12 col-md-12 article'>
                     <div className='shopContainer row'>
-                        {products.map(product=>(
+                        {Products.map(Products=>(
                             <div 
                             className='shop col-lf-4 col-md-6 col-sm-6'
-                            // key={product._id}
-                            key={product}
+                            key={Products._id}
+                            // key={product}
                             >
                                 <div className='border-product'>
                                     <Link to={'/products/${product._id'}>
                                     {/* <Link to={'/products/${product'}> */}
                                         <div className='shopBack'>
-                                            <img src={product.image} alt={product.name}/>
+                                            <img src={Products.image} alt={Products.name}/>
                                             </div>
                                     </Link>
                                     <div className='shoptext'>
                                         <p>
-                                            {/* <Link to={`/products/${product._id}`}
-                                            // {...product.name}
+                                            {/* <Link to={`/Productss/${Products._id}`}
+                                            // {...Products.name}
                                             </Link> */}
                                             </p>
                                             <Rating 
-                                            value ={product.rating}
-                                            text= {`${product.numReviews} reviews`}/>
-                                            <h3>${product.price}</h3>
+                                            value ={Products.rating}
+                                            text= {`${Products.numReviews} reviews`}/>
+                                            <h3>${Products.price}</h3>
                                             </div>
                                     </div>
                                     </div>
