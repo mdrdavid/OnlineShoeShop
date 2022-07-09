37931651c85d25d7b2d9d9355b8857e872fba5a5
david <davidmatovu88@gmail.com>
2022-07-09 19:05:39 +0300

    Profile page

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,67 +1,69 @@
 import React from 'react'
 import Header from "../../components/profileComponents/mainheader/Header"
-import Orders from "../orders/OrderSection"
-import ProfileTabs from "../../components/profileComponents/ProfileTabs"
+// import Orders from "../orders/OrderSection"
+import ProfileTabs from "../../components/profileComponents/profiletabs/ProfileTabs"
 import "./profile.css"
 
 
-const ProfilePage= ()=> {
+const ProfilePage = () => {
+
   return (
     <>
-    <Header/>
-    <div className='conatainer'>
-      <div className='row align-items-start'>
-        <div className='col-lg-4 shadow'>
-          <div className='author-card-cover'></div>
-          <div className='author-card-avatar'>
-            <img src='image' alt='userprofileimage'/>
+      <Header />
+      <div className='profile-conatainer'>
+        <div className='profile-avator'>
+          <div className='avator'>
+            <div className='author-card-avatar'>
+              <img src='/assets/images/david.jpg' alt='userprofileimage' 
+              style={{width: "80px", height:"50"}}/>
+            </div>
+            <div className='author-card-details'>
+              <h5 className='author-card-name'>
+                <strong>Admin David</strong>
+              </h5>
+              <span className='author-card-position'>
+                <>Joined Dec 12 2021</>
+              </span>
+            </div>
           </div>
-          <div className='author-card-details'>
-            <h5 className='author-card-name'>
-              <strong>Admin David</strong>
-            </h5>
-            <span className='author-card-position'>
-              <>Joined Dec 12 2021</>
-            </span>
+          <div className='wizard'>
+            <div className='d-flex align-items-center'>
+              <div
+                className='nav flex-colum items-start'
+                id="v-pills-tab"
+                role="tablist"
+                aria-orientation='vertical'>
+                <button
+                  className='nav-link active'
+                  id='v-pills-home-tab'
+                  data-bs-target="v-pills-home"
+                  type='button'
+                  role="tab"
+                  aria-controls="v-pills-home"
+                  aria-selected="true">
+                  Profile Settings
+                </button>
+                <button
+                  className='nav-link d-flex justify-content-between'
+                  id='v-pills-home-tab'
+                  data-bs-target="pills"
+                  type='button'
+                  role="tab"
+                  aria-controls="v-pills-profile"
+                  aria-selected="false">
+                  Orders List
+                  <span className='badget2'>3</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='wizard'>
-        <div className='d-flex align-items-center'>
-          <div
-          className='nav flex-colum items-start'
-          id="v-pills-tab"
-          role="tablist"
-          aria-orientation='vertical'>
-            <button
-            className='nav-link active'
-            id='v-pills-home-tab'
-            data-bs-target="v-pills-home"
-            type='button'
-            role="tab"
-            aria-controls="v-pills-home"
-            aria-selected="true">
-              Profile Settings
-            </button>
-            <button
-            className='nav-link d-flex justify-content-between'
-            id='v-pills-home-tab'
-            data-bs-target="pills"
-            type='button'
-            role="tab"
-            aria-controls="v-pills-profile"
-            aria-selected="false">
-              Orders List
-              <span className='badget2'>3</span>
-            </button>
-          </div>
+        <div className='tab-content'
+          id='v-pills-tabcontent'>
+          <ProfileTabs />
         </div>
       </div>
-    </div>
-    <div className='tab-content'
-    id='v-pills-tabcontent'>
-      <ProfileTabs/>
-    </div>
+
     </>
   )
 }
