5e68b697a649e6b4ae05c3b72330e8cc96d44500
david <davidmatovu88@gmail.com>
2022-07-22 15:33:26 +0300

    Get single product

--- backend/Routes/ProductRoutes.js
+++ backend/Routes/ProductRoutes.js
@@ -4,6 +4,7 @@ import Product from "../Models/ProductModel.js"
 
 const productRoute = express.Router()
 
+// GET ALL PRODUCTS
 productRoute.get("/", asyncHandler(
     async(req, res) =>{
         const products = await Product.find({})
@@ -12,4 +13,17 @@ productRoute.get("/", asyncHandler(
 )
 );
 
+//  GET SINGLE PRODUCT
+productRoute.get("/:id", asyncHandler(
+    async(req, res) =>{
+        const product = await Product.findById(product.params.id)
+        if (product) {
+            res.json(product)
+        } else {
+            res.status(404)
+            throw new Error("Product not found")
+        }
+    }
+)
+);
 export default productRoute
