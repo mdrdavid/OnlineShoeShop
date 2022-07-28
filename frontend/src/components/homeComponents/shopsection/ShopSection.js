a13aeb0c7fbf7eab32824cf4ad906b49bd10ae5a
david <davidmatovu88@gmail.com>
2022-07-28 15:43:08 +0300

    Moved product componet out

--- frontend/src/components/homeComponents/shopsection/ShopSection.js
+++ frontend/src/components/homeComponents/shopsection/ShopSection.js
@@ -1,45 +1,13 @@
 import React ,{useEffect}from 'react'
-import Rating from '../Rating';
 import Loading from "../../loadingError/Loading"
 import Message from "../../loadingError/Error"
-import { Link } from 'react-router-dom'
 import {useDispatch, useSelector } from "react-redux"
 import "./shopsection.css"
 import { listProduct} from '../../../Redux/Actions/ProductActions'
+import { Product } from './Product';
 
-const Product = (props) => {
-    console.log(props)
-    const { product } = props
-    console.log(product.imageUrl || product.imageFile || product.image)
-    return (
-        <div
-            className='shop'
-            key={product._id} >
-            <div className='border-product'>
-                <Link to={`/products/${product._id}`}>
-                    <div className='shopBack'>
-                        <img src={product.imageUrl || product.imageFile || product.image} alt={product.name}
-                            style={{ width: "100%", height: "200px" }} />
-                    </div>
-                </Link>
-                <div className='shoptext'>
-                    <p>
-                        <Link to={`/products/${product._id}`}>
-                            {product.name}
-                        </Link>
-                    </p>
-                    <Rating
-                        value={product.rating}
-                        text={`${product.numReviews} reviews`}
-                        image={product.imageFile} />
-                    <h3>${product.price}</h3>
-                </div>
-            </div>
-        </div>
-    )
-}
+
 const ShopSection = () => {
-    // const [products, setProducts] =useState({})
     const dispatch = useDispatch()
 
     //productList from store.js
@@ -47,12 +15,6 @@ const ShopSection = () => {
     const {loading, error, products}= productList
     useEffect(()=>{
         dispatch(listProduct())
-
-// const fetchproduct =async ()=>{
-// const {data} = await axios.get(`${URL}/api/products`)
-// setProducts(data)
-// };
-// fetchproduct();
     },[dispatch])
 
     return (
