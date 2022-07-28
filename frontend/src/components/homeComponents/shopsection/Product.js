ae961e520685190fb672312d30d8f91b28279b7c
david <davidmatovu88@gmail.com>
2022-07-28 15:43:52 +0300

    create product component

---
+++ frontend/src/components/homeComponents/shopsection/Product.js
@@ -0,0 +1,34 @@
+
+import Rating from '../Rating';
+import { Link } from 'react-router-dom'
+export const Product = (props) => {
+    console.log(props)
+    const { product } = props
+    console.log(product.imageUrl || product.imageFile || product.image)
+    return (
+        <div
+            className='shop'
+            key={product._id} >
+            <div className='border-product'>
+                <Link to={`/products/${product._id}`}>
+                    <div className='shopBack'>
+                        <img src={product.imageUrl || product.imageFile || product.image} alt={product.name}
+                            style={{ width: "100%", height: "200px" }} />
+                    </div>
+                </Link>
+                <div className='shoptext'>
+                    <p>
+                        <Link to={`/products/${product._id}`}>
+                            {product.name}
+                        </Link>
+                    </p>
+                    <Rating
+                        value={product.rating}
+                        text={`${product.numReviews} reviews`}
+                        image={product.imageFile} />
+                    <h3>${product.price}</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
